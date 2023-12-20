f21ba3eb52e8256a40518f4ed33d39e7110daf13
Tomiwa Bada <tomiwabd@gmail.com>
2023-12-20 23:05:26 +0100

    Added scroll trigger

--- index.js
+++ index.js
@@ -10,6 +10,63 @@ gsap.to(".char", {
   duration: 0.5,
 });
 
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to(".shop-item", {
+  scrollTrigger: {
+    trigger: ".first-trigger",
+    start: "top bottom",
+    end: "center center",
+    scrub: 1,
+    markers: false,
+  },
+  x: 0,
+  opacity: 1,
+  duration: 1,
+  stagger: 0.1,
+});
+
+gsap.to(".shop-item-2", {
+  scrollTrigger: {
+    trigger: ".second-trigger",
+    start: "top bottom",
+    end: "center center",
+    scrub: 1,
+    markers: false,
+  },
+  x: 0,
+  opacity: 1,
+  duration: 1,
+  stagger: 0.2,
+});
+
+gsap.to(".shop-item-3", {
+  scrollTrigger: {
+    trigger: ".second-trigger",
+    start: "top bottom",
+    end: "center center",
+    scrub: 1,
+    markers: false,
+  },
+  x: 0,
+  opacity: 1,
+  duration: 1,
+  stagger: 0.2,
+});
+
+gsap.to(".shop-item-4", {
+  scrollTrigger: {
+    trigger: ".third-trigger",
+    start: "top bottom",
+    end: "center center",
+    scrub: 1,
+    markers: false,
+  },
+  x: 0,
+  opacity: 1,
+  duration: 1,
+});
+
 gsap.to(".flow-item", {
   duration: 10,
   ease: "linear",
@@ -35,4 +92,21 @@ document.querySelector("body").addEventListener("mouseover", (e) => {
 });
 document.querySelector("body").addEventListener("mouseout", (e) => {
   cursor.classList.remove("cursor-active");
+});
+
+document.querySelector("#hamburger").addEventListener("click", () => {
+  if (!document.querySelector("nav").classList.contains("is-active")) {
+    document.querySelector("nav").classList.add("is-active");
+    document.querySelector(".hamburger-line").style.width = "2.2rem";
+    // window.onscroll = function () {};
+  } else {
+    document.querySelector("nav").classList.remove("is-active");
+    document.querySelector(".hamburger-line").style.width = "1.6rem";
+    // document.querySelector("body").style.position = "fixed";
+    // scrollTop = window.scrollY || document.documentElement.scrollTop;
+    // (scrollLeft = window.scrollX || document.documentElement.scrollLeft),
+    //   (window.onscroll = function () {
+    //     window.scrollTo(scrollLeft, scrollTop);
+    //   });
+  }
 });
