8e3ed5b12b00e3f343b2791e36aab01cf842c9c0
Tomiwa Bada <tomiwabd@gmail.com>
2024-01-02 23:49:06 +0100

    Added animation to sales page

---
+++ bestselling.js
@@ -0,0 +1,33 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let sections = gsap.utils.toArray(".best-selling-item");
+
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".best-selling-container",
+    pin: true,
+    scrub: 1,
+    snap: 1 / (sections.length - 1),
+    end: () =>
+      "+=" + document.querySelector(".best-selling-container").offsetWidth,
+  },
+});
+
+let cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.pageX - cursor.offsetWidth + "px"; // Adjust the offset
+  cursor.style.top = e.pageY - cursor.offsetHeight + "px";
+});
+
+document.querySelector("body").addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("shop-item-image")) {
+    cursor.classList.add("cursor-active");
+  } else {
+    cursor.classList.remove("cursor-active");
+  }
+});
+document.querySelector("body").addEventListener("mouseout", (e) => {
+  cursor.classList.remove("cursor-active");
+});
