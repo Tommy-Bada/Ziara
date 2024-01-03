e5b3ce2fca2d008cc6588ccf96aa8dfef789d178
Tomiwa Bada <tomiwabd@gmail.com>
2024-01-03 21:58:26 +0100

    Fixed overflow bug on nav

--- nav.js
+++ nav.js
@@ -2,16 +2,18 @@ document.querySelector("#hamburger").addEventListener("click", () => {
   if (!document.querySelector("nav").classList.contains("is-active")) {
     document.querySelector("nav").classList.add("is-active");
     document.querySelector(".hamburger-line").style.width = "2.2rem";
+    document.querySelector("body").style.position = "fixed";
   } else {
     document.querySelector("nav").classList.remove("is-active");
     document.querySelector(".hamburger-line").style.width = "1.6rem";
+    document.querySelector("body").style.position = "static";
   }
 });
 
 let menuItem = document.querySelectorAll(".nav-text");
 let menuImage = document.querySelectorAll(".nav-image");
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 5; i++) {
   const animation = gsap.to(menuImage[i], {
     opacity: 1,
     duration: 0.2,
