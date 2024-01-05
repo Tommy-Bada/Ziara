0f1cc7bcee1db6af66f71dcadf580eb7f8988f3d
Tomiwa Bada <tomiwabd@gmail.com>
2024-01-05 23:19:22 +0100

    Started page transition animation

--- bestselling.js
+++ bestselling.js
@@ -1,7 +1,33 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let sections = gsap.utils.toArray(".best-selling-item");
+menuItem = document.querySelectorAll(".nav-text");
+menuItem[3].addEventListener("click", () => {
+  console.log(menuItem[3]);
+  gsap.to("nav", {
+    y: "-100vh",
+    duration: 0.1,
+    ease: "ease-in-out",
+  });
+});
 
+let tl = gsap.timeline();
+tl.to(".prepage-inner", {
+  delay: 0.5,
+  height: "100vh",
+  duration: 1,
+  ease: "ease-in-out",
+});
+tl.to(".prepage", {
+  y: "-100vh",
+  delay: 0.5,
+  duration: 1,
+  onComplete: function () {
+    document.querySelector(".prepage").style.display = "none";
+    document.querySelector("body").classList.remove("fixed");
+  },
+});
+
+let sections = gsap.utils.toArray(".best-selling-item");
 gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
   ease: "none",
