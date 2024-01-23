f5bf24897d08d53bde0623fd430ed7981c58378c
Tomiwa Bada <tomiwabd@gmail.com>
2024-01-24 00:46:47 +0100

    Adjusted nav for path exception

--- nav.js
+++ nav.js
@@ -2,7 +2,7 @@ document.querySelector("#hamburger").addEventListener("click", () => {
   if (!document.querySelector("nav").classList.contains("is-active")) {
     document.querySelector("nav").classList.add("is-active");
     document.querySelector(".hamburger-line").style.width = "2.2rem";
-    // document.querySelector("body").style.position = "fixed";
+    document.querySelector("body").style.position = "fixed";
   } else {
     document.querySelector("nav").classList.remove("is-active");
     document.querySelector(".hamburger-line").style.width = "1.6rem";
@@ -10,8 +10,8 @@ document.querySelector("#hamburger").addEventListener("click", () => {
   }
 });
 
-let menuItem = document.querySelectorAll(".nav-text");
-let menuImage = document.querySelectorAll(".nav-image");
+var menuItem = document.querySelectorAll(".nav-text");
+var menuImage = document.querySelectorAll(".nav-image");
 
 for (let i = 0; i < 5; i++) {
   const animation = gsap.to(menuImage[i], {
@@ -37,4 +37,18 @@ function moveText(e) {
 
 menuItem.forEach((el) => {
   el.addEventListener("mousemove", moveText);
+});
+
+document.querySelector("ul").addEventListener("click", function (event) {
+  if (event.target.parentElement.classList.contains("nav-menu")) {
+    if (
+      event.target.parentElement.getAttribute("href") ===
+      window.location.pathname
+    ) {
+      event.preventDefault();
+      document.querySelector("nav").classList.remove("is-active");
+      document.querySelector(".hamburger-line").style.width = "1.6rem";
+      document.querySelector("body").style.position = "static";
+    }
+  }
 });
