2210fdcc167409b124abc2b387515b218f49d1ca
Tomiwa Bada <tomiwabd@gmail.com>
2024-01-24 23:42:13 +0100

    Updated homepage animations

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
 gsap.registerPlugin(ScrollTrigger);
-gsap.to(".shop-item", {
+gsap.from(".shop-item", {
   scrollTrigger: {
     trigger: ".first-trigger",
     start: "top bottom",
     end: "center center",
-    scrub: 0.5,
+    scrub: 1,
     markers: false,
   },
-  y: 0,
-  opacity: 1,
-  duration: 2,
+  opacity: 0,
+  y: 150,
   stagger: 0.1,
   ease: "power1.inOut",
+  duration: 3,
 });
 
-gsap.to(".shop-item-2", {
-  scrollTrigger: {
-    trigger: ".second-trigger",
-    start: "top bottom",
-    end: "center center",
-    scrub: 0.5,
-    markers: false,
-  },
-  y: 0,
-  opacity: 1,
-  duration: 2,
-  stagger: 0.2,
-  ease: "power1.inOut",
-});
-
-gsap.to(".shop-item-3", {
+gsap.from(".shop-item-2", {
   scrollTrigger: {
     trigger: ".second-trigger",
     start: "top bottom",
@@ -37,24 +22,38 @@ gsap.to(".shop-item-3", {
     scrub: 1,
     markers: false,
   },
-  y: 0,
-  opacity: 1,
+  y: 150,
+  opacity: 0,
   duration: 2,
   stagger: 0.2,
   ease: "power1.inOut",
 });
 
-gsap.to(".shop-item-4", {
+gsap.from(".shop-item-3", {
+  scrollTrigger: {
+    trigger: ".second-trigger",
+    start: "top bottom",
+    end: "center center",
+    scrub: 1,
+    markers: false,
+  },
+  y: 150,
+  opacity: 0,
+  duration: 2,
+  stagger: 0.2,
+  ease: "power1.inOut",
+});
+
+gsap.from(".shop-item-4", {
   scrollTrigger: {
     trigger: ".third-trigger",
     start: "top bottom",
     end: "center center",
-    scrub: 1,
     markers: false,
   },
-  x: 0,
-  opacity: 1,
-  duration: 1,
+  x: 100,
+  opacity: 0,
+  duration: 2,
   ease: "power1.inOut",
 });
 
@@ -86,15 +85,38 @@ document.querySelector("body").addEventListener("mouseout", (e) => {
 });
 
 var container = document.querySelector(".last-scroll-container");
+var currentPosition = 0;
 
 document
   .querySelector(".right-arrow-btn")
   .addEventListener("click", function () {
-    container.scrollLeft += "200";
+    currentPosition -= 410;
+    if (currentPosition < -2460) {
+      currentPosition = 0;
+      gsap.to(".shop-item-4", {
+        x: 0,
+        duration: 1,
+      });
+    } else {
+      gsap.to(".shop-item-4", {
+        x: currentPosition,
+        duration: 1,
+        stagger: 0.1,
+        ease: "power2.out",
+      });
+    }
   });
 
 document
   .querySelector(".left-arrow-btn")
   .addEventListener("click", function () {
-    container.scrollLeft -= "200";
+    // currentPosition += 410;
+    gsap.to(".shop-item-4", {
+      x: 0,
+      duration: 2,
+      ease: "power2.in",
+      onComplete: () => {
+        currentPosition = 0;
+      },
+    });
   });
