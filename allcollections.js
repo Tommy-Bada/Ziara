9180288c849afbb93e69802565a053dc01bbc2cf
Tomiwa Bada <tomiwabd@gmail.com>
2024-01-25 17:20:30 +0100

    Updated all collections page

---
+++ allcollections.js
@@ -0,0 +1,34 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var sections = gsap.utils.toArray(".best-selling-item");
+if (window.matchMedia("(min-width: 720px)").matches) {
+  gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".best-selling-container",
+      pin: true,
+      scrub: 1,
+      snap: 1 / (sections.length - 1),
+      end: () =>
+        "+=" + document.querySelector(".best-selling-container").offsetWidth,
+    },
+  });
+}
+
+var cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.pageX - cursor.offsetWidth + "px"; // Adjust the offset
+  cursor.style.top = e.pageY - cursor.offsetHeight + "px";
+});
+
+document.querySelector("body").addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("shop-item-image")) {
+    cursor.classList.add("cursor-active");
+  } else {
+    cursor.classList.remove("cursor-active");
+  }
+});
+document.querySelector("body").addEventListener("mouseout", (e) => {
+  cursor.classList.remove("cursor-active");
+});
